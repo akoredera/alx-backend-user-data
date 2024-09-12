17b6f2ffda1a0f37a74e4e3a3d2add1748c00545
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-09-12 12:27:20 +0000

    modified db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,6 +34,8 @@ class DB:
         '''
         method should save the user to the database
         '''
+        if not email and hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
