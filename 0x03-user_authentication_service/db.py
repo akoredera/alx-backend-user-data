cddb22907f0217f37b8d82d6c2de416a6f490169
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-09-12 13:15:22 +0000

    modified db file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,7 +45,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         '''
         This method takes in arbitrary keyword arguments and returns
         the first row found in the users table as filtered
