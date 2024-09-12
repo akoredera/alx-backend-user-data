6e0ce0c02d80e78d54b850edcfdcf360a5696ca9
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-09-12 14:34:42 +0000

    modified file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 auth
 '''
 import bcrypt
-
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 def _hash_password(password: str) -> bytes:
     '''
@@ -13,3 +15,20 @@ def _hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        '''constructor'''
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''register new user'''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -74,3 +74,23 @@ from auth import _hash_password
 
 print(_hash_password("Hello Holberton"))
 
+
+
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
